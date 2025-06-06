a91131726fb7671302e83dd9fbaeb086bf241aac
victoroseghale <victor.oseghale@datadoghq.com>
2025-06-06 21:31:17 +0200

    Implemented delete dao

--- src/main/java/com/ddlabs/atlassian/impl/data/ServerConfigRepositoryImpl.java
+++ src/main/java/com/ddlabs/atlassian/impl/data/ServerConfigRepositoryImpl.java
@@ -36,6 +36,6 @@ public class ServerConfigRepositoryImpl implements ServerConfigRepository {
 
     @Override
     public void delete(String serverType) throws DataAccessException {
-
+        pluginDaoRepository.deleteServerConfig(serverType);
     }
 }
